6750e70d199f6a8ecd833cab54479b8e1dfbec00
小飞飞 <2237928292@qq.com>
2023-10-17 17:52:51 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import './index.scss'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
---
+++ src/views/login/index.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class='box'>
+        SCSS
+
+        <el-row>
+            <el-button>默认按钮</el-button>
+            <el-button type="primary">主要按钮</el-button>
+            <el-button type="success">成功按钮</el-button>
+            <el-button type="info">信息按钮</el-button>
+            <el-button type="warning">警告按钮</el-button>
+            <el-button type="danger">危险按钮</el-button>
+        </el-row>
+
+        <img src="../../assets/images/logo.png" alt="" style="width: 500px;">
+    </div>
+</template>
+
+<script>
+// 这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
+export default {
+    // 定义属性
+    data() {
+        return {
+
+        }
+    },
+    // 生命周期 - 创建完成（可以访问当前this实例）
+    created() {
+
+    },
+    // 生命周期 - 挂载完成（可以访问DOM元素）
+    mounted() {
+
+    },
+    // 计算属性，会监听依赖属性值随之变化
+    computed: {
+    },
+    // 监控data中的数据变化
+    watch: {},
+    // 方法集合
+    methods: {
+
+    },
+    beforeCreate() { }, // 生命周期 - 创建之前
+    beforeMount() { }, // 生命周期 - 挂载之前
+    beforeUpdate() { }, // 生命周期 - 更新之前
+    updated() { }, // 生命周期 - 更新之后
+    beforeDestroy() { }, // 生命周期 - 销毁之前
+    destroyed() { }, // 生命周期 - 销毁完成
+    activated() { }, // 如果页面有keep-alive缓存功能，这个函数会触发
+}
+</script>
+
+<style lang='scss' scoped>
+.box {
+    width: 100%;
+    height: 100%;
+    background: #e3dede;
+}
+</style>
